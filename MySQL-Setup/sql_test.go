07b96ef1a10b1d1f94af35b6543a4f7eee83a708
Andhika-GIT <andhikapr081@gmail.com>
2023-11-26 18:52:29 +0700

    Mysql: bad way to write sql query, high potential of sql injection

--- MySQL-Setup/sql_test.go
+++ MySQL-Setup/sql_test.go
@@ -40,3 +40,41 @@ func TestExecSqlQuery(t *testing.T) {
 	// fmt.Println("successfully insert to database")
 
 }
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "salah"
+
+	selectQuery := "SELECT username FROM user WHERE username = '" + username + "' AND password = '" + password + "' LIMIT 1"
+	rows, err := db.QueryContext(ctx, selectQuery)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	// looping through all data rows
+	if rows.Next() {
+		var username string
+
+		err := rows.Scan(&username)
+
+		// check if error
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("username : %v", username)
+	} else {
+		fmt.Println("gagal login")
+	}
+
+	// fmt.Println("successfully insert to database")
+
+}
