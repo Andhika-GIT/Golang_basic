3956967bff71a0b0b0fd4092b6a3b26ad496c496
Andhika Prasetya <andhikapr081@gmail.com>
2023-11-28 11:10:09 +0700

    Server: ServerMux for handling multiple routes

--- Server/hander_test.go
+++ Server/hander_test.go
@@ -24,3 +24,32 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "hello world")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "this is hi page")
+	})
+	mux.HandleFunc("/images", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "this is images page")
+	})
+
+	mux.HandleFunc("/images/thumbnail", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "this is thumbnail page")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
