607f496d2246195acc5e8da9d8b609302be596b6
Andhika Prasetya <andhikapr081@gmail.com>
2023-11-28 12:40:53 +0700

    Server: http test

--- Server/hander_test.go
+++ Server/hander_test.go
@@ -2,7 +2,9 @@ package Server
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -75,4 +77,24 @@ func TestRequestHandler(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func helloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "hello world")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	helloHandler(recorder, request)
+
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+
+	bodyString := string(body)
+
+	fmt.Println("status code : ", response.StatusCode)
+	fmt.Println("body : ", bodyString)
 }
